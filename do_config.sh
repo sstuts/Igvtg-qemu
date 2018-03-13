9b9742bab50df61076bc9dfb9fd0d4dc0c05decf
sandy <sstuts@gmail.com>
2018-03-13 15:44:30 -0400

    update build path

--- do_config.sh
+++ do_config.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./configure --extra-ldflags="-L/usr/lib" --prefix=/usr/lib/xen --enable-xen-410 --enable-xen --disable-kvm --enable-debug-info --enable-debug --enable-sdl --enable-vhost-net --disable-debug-tcg --target-list=x86_64-softmmu --enable-opengl 
+./configure --extra-ldflags="-L/usr/lib/xen/lib" --prefix=/usr/lib/xen --enable-xen-410 --enable-xen --disable-kvm --enable-debug-info --enable-debug --enable-sdl --enable-vhost-net --disable-debug-tcg --target-list=x86_64-softmmu --enable-opengl --enable-spice
